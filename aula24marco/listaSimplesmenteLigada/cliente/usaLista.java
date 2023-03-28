f619f3d384a579ecc3fac1227505fcf28483cbba
iansaggior <110851341+iansaggior@users.noreply.github.com>
2023-03-28 08:55:36 -0300

    Update usaLista.java

--- aula24marco/listaSimplesmenteLigada/cliente/usaLista.java
+++ aula24marco/listaSimplesmenteLigada/cliente/usaLista.java
@@ -6,18 +6,21 @@ import listaSimplesmenteLigada.lista.Lista;
 
 public class usaLista {
     public static void main(String[] args) {
+
         //INSERINDO OBJETOS NA LISTA
         Lista lista = new Lista();
-/*          lista.inserirContato(new Contato("Gerson", "1234-5678"));
+        
+/*         lista.inserirContato(new Contato("Gerson", "1234-5678"));
         lista.inserirContato(new Contato("Mia", "5432-5678"));
         lista.inserirContato(new Contato("Anna", "1234-8520"));
         lista.inserirContato(new Contato("Claudio", "1597-3547"));
         lista.inserirContato(new Contato("José", "6230-5071"));
-        lista.inserirContato(new Contato("Nair", "5498-3265"));  
+        lista.inserirContato(new Contato("Nair", "5498-3265"));
+
         //MOSTRAR Os OBJETOS
-        lista.exibirContatos();*/
+        lista.exibirContatos(); */
         
-        lista.inserirProduto(new Produto(20,"Sabonete", "Higiene Pessoal"));
+/*         lista.inserirProduto(new Produto(20,"Sabonete", "Higiene Pessoal"));
         lista.inserirProduto(new Produto(15,"Maçã", "Alimentícia"));
         lista.inserirProduto(new Produto(29.99,"Desodorante", "Higiene Pessoal"));
         lista.inserirProduto(new Produto(17,"Água Saniária", "Limpeza"));
@@ -36,7 +39,7 @@ public class usaLista {
 
         System.out.println("DEPOIS DO DESCONTO");
         //MOSTRAR Os OBJETOS
-        lista.exibirProdutos();
+        lista.exibirProdutos(); */
 
 /*          //BUSCAR ELEMENTO PELO ID (Contato)
         int buscarIdContato = 4;
@@ -56,7 +59,7 @@ public class usaLista {
         //Object obj = lista.pesquisarIdProduto(buscarIdProduto);
         // lista.pesquisarId(buscarIdProduto) é a mesma coisa que obj
         if (lista.pesquisarIdProduto(buscarIdProduto) != null) {
-            Contato elemento = (Contato) lista.pesquisarIdProduto(buscarIdProduto);
+            Produto elemento = (Produto) lista.pesquisarIdProduto(buscarIdProduto);
             System.out.println("\nElemento encontrado");
             System.out.println(elemento);
         }
